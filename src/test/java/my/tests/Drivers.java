27e97b61a466d1e895d99e5811ae564687f01a70
lenagithub4 <jelena.uscelemova@aaro.se>
2020-02-24 22:54:27 +0200

    test

---
+++ src/test/java/my/tests/Drivers.java
@@ -0,0 +1,58 @@
+package my.tests;
+
+import org.junit.After;
+        import org.junit.Before;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.firefox.FirefoxDriver;
+        import org.openqa.selenium.ie.InternetExplorerDriver;
+        import org.openqa.selenium.remote.DesiredCapabilities;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class Drivers {
+    public static WebDriver wd;
+    public WebDriverWait wait;
+
+    @Before
+    public void start(){
+        if (wd==null){
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setBrowserName("chrome");
+            wd = new ChromeDriver(caps);
+            wait = new WebDriverWait(wd, 30);
+        }
+
+        //firefox
+        // if (wd==null){
+        //  DesiredCapabilities caps = new DesiredCapabilities();
+        // caps.setCapability(FirefoxDriver.MARIONETTE, true);
+        //  wd = new FirefoxDriver(caps);
+        // wd.manage().window().maximize();
+        // wait = new WebDriverWait(wd, 30);
+        //   System.setProperty("webdriver.gecko.driver", "C:\\Users\\user\\Documents\\Selenium WEBDriver\\geckodriver\\geckodriver.exe");
+        //   wd = new FirefoxDriver();
+        //   wait = new WebDriverWait(wd, 2000);
+        // }
+
+        //ie
+        //if (wd==null) {
+        //DesiredCapabilities caps = new DesiredCapabilities();
+        //caps.setCapability(InternetExplorerDriver.REQUIRE_WINDOW_FOCUS, true);
+        //caps.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false);
+        // caps.setCapability(InternetExplorerDriver.IGNORE_ZOOM_SETTING, true);
+        // wd = new InternetExplorerDriver(caps);
+        // wd.manage().window().maximize();
+        // wait = new WebDriverWait(wd, 30);
+        // }
+
+        //System.out.println(((HasCapabilities) wd).getCapabilities());
+
+        //wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //wait = new WebDriverWait(wd, 10);
+    }
+
+    @After
+    public void quit(){
+        wd.quit();}
+}
