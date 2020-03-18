60e3807cc8923a09f886983630256dfb8a2aad8c
lenagithub4 <jelena.uscelemova@aaro.se>
2020-03-18 22:52:59 +0200

    test

--- src/test/java/my/tests/Drivers.java
+++ src/test/java/my/tests/Drivers.java
@@ -17,28 +17,29 @@ public class Drivers  {
 
     @Before
     public void start() {
-        //chrome
-        if (drv == null) {
-            DesiredCapabilities caps = new DesiredCapabilities();
-          caps.setBrowserName("chrome");
-            drv = new ChromeDriver(caps);
-          wait = new WebDriverWait(drv, 2000);
+
+        //Chrome
+       if (drv == null) {
+           DesiredCapabilities caps = new DesiredCapabilities();
+           caps.setBrowserName("chrome");
+           drv = new ChromeDriver(caps);
+           wait = new WebDriverWait(drv, 2000);
        }
 
-        //firefox
-        // if (drv==null){
-        // System.setProperty("webdriver.gecko.driver", "C:/Users/user/Documents/Selenium WEBDriver/geckodriver/geckodriver.exe");
-       // drv = new FirefoxDriver();
-        // wait = new WebDriverWait(drv, 2000);
+        //Firefox
+         //if (drv==null){
+         // System.setProperty("webdriver.gecko.driver", "C:/Users/user/Documents/Selenium WEBDriver/geckodriver/geckodriver.exe");
+         //drv = new FirefoxDriver();
+         //wait = new WebDriverWait(drv, 2000);
         //}
 
-        //ie
-       // if (drv==null) {
-       //  DesiredCapabilities caps = new DesiredCapabilities();
-       //  drv = new InternetExplorerDriver(caps);
-       //  drv.manage().window().maximize();
-       //   wait = new WebDriverWait(drv, 30);
-      //  }
+        //IE
+        //if (drv==null) {
+         //DesiredCapabilities caps = new DesiredCapabilities();
+         //drv = new InternetExplorerDriver(caps);
+         //drv.manage().window().maximize();
+          //wait = new WebDriverWait(drv, 30);
+       // }
     }
 
     @After
