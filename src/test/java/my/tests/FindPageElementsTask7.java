08ee5f35c049e540eb89846163c025f6363a9008
lenagithub4 <jelena.uscelemova@aaro.se>
2020-03-24 23:02:33 +0200

    test

--- src/test/java/my/tests/FindPageElementsTask7.java
+++ src/test/java/my/tests/FindPageElementsTask7.java
@@ -25,50 +25,51 @@ public class FindPageElementsTask7 extends Drivers {
             WebElement menuAdminkaElement = menuAdminkaList.get(i);
            // menuHeader = drv.findElements(By.id("text"));
 
-            // print of menu element
+            // print of menu element on what I will click
             System.out.println("Element of menu: " + menuAdminkaList.get(i).getAttribute("outerText") );
 
             //click on menu Element
             menuAdminkaElement.click();
 
-            if (drv.getTitle().equals("")){
-                AssertionError assertError = new AssertionError();
-                System.out.println("WARNING! No Title for Page: " + drv.getCurrentUrl()+ " ." +assertError.getMessage());
-                Assert.fail();
-            }
-
-            // что я просмотрела / список
-
-            System.out.println("    Page Title: " + drv.getTitle());
-            System.out.println(" --------------------------- " );
-            drv.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-
-
-
-             // submenu
-            if ((drv.findElements(By.cssSelector("ul#box-apps-menu li#app-.selected ul.docs li")).size() > 0)) {
-                List<WebElement> subMenuAdminkaList = drv.findElements(By.cssSelector("ul#box-apps-menu li#app- ul.docs li"));
-
-                for (int j = 0; j < subMenuAdminkaList.size(); j++) {
-                    subMenuAdminkaList = drv.findElements(By.cssSelector("ul#box-apps-menu li#app- ul.docs li"));
-                    WebElement subMenuAdminkaElement = subMenuAdminkaList.get(j);
-                   // menuHeader = drv.findElements(By.id("text"));
-                    System.out.println("   Element of subMenu: " + subMenuAdminkaList.get(j).getAttribute("outerText") );
-                    subMenuAdminkaElement.click();
-
                     if (drv.getTitle().equals("")){
                         AssertionError assertError = new AssertionError();
-                        System.out.println("WARNING! No Title for Page:" + drv.getCurrentUrl()+ " ." +assertError.getMessage());
+                        System.out.println("WARNING! No Title for Page: " + drv.getCurrentUrl()+ " ." +assertError.getMessage());
                         Assert.fail();
                     }
 
-                    System.out.println("       subPage Title: " + drv.getTitle());
-                    System.out.println(" --------------------------- " );
-                    drv.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); // вызываю, пока не появится элемент
-                }
-                drv.navigate().to("http://localhost/litecart/admin");
-                menuAdminkaList = drv.findElements(By.cssSelector("ul#box-apps-menu li#app-"));
-            }
+                // что я просмотрела / список
+
+                System.out.println("    Page Title: " + drv.getTitle());
+                System.out.println(" --------------------------- " );
+                drv.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+
+
+                     // submenu
+                    if ((drv.findElements(By.cssSelector("ul#box-apps-menu li#app-.selected ul.docs li")).size() > 0)) {
+                        List<WebElement> subMenuAdminkaList = drv.findElements(By.cssSelector("ul#box-apps-menu li#app- ul.docs li"));
+
+                        for (int j = 0; j < subMenuAdminkaList.size(); j++) {
+                            subMenuAdminkaList = drv.findElements(By.cssSelector("ul#box-apps-menu li#app- ul.docs li"));
+                            WebElement subMenuAdminkaElement = subMenuAdminkaList.get(j);
+                           // menuHeader = drv.findElements(By.id("text"));
+                            System.out.println("   Element of subMenu: " + subMenuAdminkaList.get(j).getAttribute("outerText") );
+                            subMenuAdminkaElement.click();
+
+                            if (drv.getTitle().equals("")){
+                                AssertionError assertError = new AssertionError();
+                                System.out.println("WARNING! No Title for Page:" + drv.getCurrentUrl()+ " ." +assertError.getMessage());
+                                Assert.fail();
+                            }
+
+                            System.out.println("       subPage Title: " + drv.getTitle());
+                            System.out.println(" --------------------------- " );
+                            drv.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS); // вызываю, пока не появится элемент
+                        }
+                        drv.navigate().to("http://localhost/litecart/admin");
+                        menuAdminkaList = drv.findElements(By.cssSelector("ul#box-apps-menu li#app-"));
+                    }
+
         }
     }
 }
